7a68642a25dbd942a9a70415523534df1b6f567a
buZz <buzztiaan@users.noreply.github.com>
2022-05-10 11:57:21 +0200

    Fixed uvos' name

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /* 
  * This file is part of Salutem
- * Copyright (c) 2021 Carl Phlipp Klemm.
+ * Copyright (c) 2021 Carl Philipp Klemm.
  * 
  * This program is free software: you can redistribute it and/or modify  
  * it under the terms of the GNU General Public License as published by  
